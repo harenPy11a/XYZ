5a7bcfe2d05fdbf5de28ae9165d8205d9d8350cd
Haren Pylla <haren.pylla@gmail.com>
2023-08-07 11:34:14 -0500

    tailwindcss

--- App.js
+++ App.js
@@ -1,13 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import tw from 'twrnc';
+import { StatusBar } from "expo-status-bar";
+import { Text, View } from "react-native";
+import tw from "twrnc";
 
 export default function App() {
   return (
-    <View style={tw`flex-1 items-center justify-center`}>
+    <View style={tw`flex-1 items-center justify-center bg-blue-200`}>
       <Text>Have fun with this template</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
-
